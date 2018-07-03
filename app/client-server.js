198b5ca32cd23ece4fbf16e4bf136dac27355fa8
Cliff Hall <cliff@futurescale.com>
2018-07-03 14:26:25 -0400

    WIP: Building client * Changed  width of client * Added screenshot of one-on-one chat with message history * Added server for React client * Added scripts in package.json for serving the react client and for initializing and updating submodules

---
+++ app/client-server.js
@@ -0,0 +1,17 @@
+// client-server.js
+
+// Serve the client
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 8080;
+
+console.log(`Server for React instant message client started. http://localhost:${port}/`);
+app.use(express.static('app'));
+
+app.get('/', function(req, res) {
+    console.log('New client request');
+    res.sendFile(path.join(__dirname + '/react-chat-client.html'));
+});
+
+app.listen(port);
